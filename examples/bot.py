f829960c76723a8585e07eb19b60d40429006a0d
Maximilian Lotz <max-lotz@outlook.de>
2020-11-23 23:07:08 +0100

    Custom Emoji support

--- examples/bot.py
+++ examples/bot.py
@@ -4,16 +4,19 @@ from menu import MyMenu
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='!', help_command=None, intents=intents)
-
+print(client)
 
 @client.command()
 async def my_menu(ctx):
     x = [discord.Embed(title="Page 2", color=discord.Color.red()).add_field(name="Hello", value="im page two"),
          discord.Embed(title="Page 3", color=discord.Color.gold()).add_field(name="Hello", value="im page three")]
-    menu = MyMenu(client, ctx, color=000000)
+
+    menu = MyMenu(client, ctx, emoji_left=client.get_emoji(777861043803127819),
+                  emoji_right=client.get_emoji(770315754897014814), color=000000)
     menu.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
     menu.add_field(name="My menu", value="react to switch page")
-    menu.add_embeds(x)
+    menu.add_pages(x)
     await menu.start()
 
-client.run("TOKEN")
+
+client.run("")
--- menu.py
+++ menu.py
@@ -8,7 +8,7 @@ It can only be used in commands where a valid context is given.
 
 
 class MyMenu(discord.Embed):
-    def __init__(self, client, ctx, **kwargs):
+    def __init__(self, client: discord.Client, ctx, **kwargs):
         super().__init__(**kwargs)  # calls __init__ from superclass
         self.client = client
         self.ctx = ctx
@@ -17,13 +17,15 @@ class MyMenu(discord.Embed):
         self.index = 0
         self.delete = kwargs.get('delete', False)  # Specifies if the author should be able to to remove the embed
         self.timeout = kwargs.get('timeout', 60)  # Specifies the wait_for timeout defaults to 60 seconds
+        self.emoji_left = kwargs.get('emoji_left', "⏪")
+        self.emoji_right = kwargs.get("emoji_right", "⏩")
 
     async def start(self):  # Starts the menu. Sends the first page and starts to listen to reactions
-        emotes = ["⏩", "⏪"]
+        emotes = [self.emoji_left, self.emoji_right]
         try:
             message = await self.ctx.send(embed=self.embeds[self.index])
-            await message.add_reaction("⏪")
-            await message.add_reaction("⏩")
+            await message.add_reaction(self.emoji_left)
+            await message.add_reaction(self.emoji_right)
             if self.delete:
                 await message.add_reaction("🚫")
                 emotes.append("🚫")
@@ -31,7 +33,7 @@ class MyMenu(discord.Embed):
             return
 
         def check(reaction, user):
-            return user.bot is False and user.id == self.ctx.author.id and str(reaction.emoji) in emotes
+            return user.bot is False and user.id == self.ctx.author.id and reaction.emoji in emotes
 
         while True:
             try:
@@ -39,7 +41,7 @@ class MyMenu(discord.Embed):
             except asyncio.TimeoutError:
                 break
 
-            switch = {"⏩": self.move_right, "⏪": self.move_left}
+            switch = {self.emoji_right: self.move_right, self.emoji_left: self.move_left}
             try:
                 execute = switch[reaction.emoji]
                 execute()
@@ -63,7 +65,7 @@ class MyMenu(discord.Embed):
         else:
             self.index -= 1
 
-    def add_embeds(self, embed):  # Adding embeds to the menu. Parameter must be a list
+    def add_pages(self, embed):  # Adding embeds to the menu. Parameter must be a list
         for i in embed:
             if isinstance(i, discord.Embed):
                 self.embeds.append(i)
